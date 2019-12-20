e4630f2420645f9c5517991d343cfc4d68690ad0
daveSelhime <41130208+daveSelhime@users.noreply.github.com>
2019-12-20 12:59:16 -0700

    added length function

--- public/JS/validate.js
+++ public/JS/validate.js
@@ -3,6 +3,11 @@ console.log(validationBox)
 // get password input on focus
 let pwdInput = document.querySelector('#password');
 console.log(pwdInput)
+let characters = document.querySelector('.character');
+let length = document.querySelector('.length');
+let number = document.querySelector('.number');
+
+
 // add display block to validation-box
 pwdInput.onfocus = function() {
     validationBox.style.display = "block"
@@ -11,3 +16,13 @@ pwdInput.onfocus = function() {
 pwdInput.onblur = function() {
  validationBox.style.display = "none"
 };
+
+// When user types into input
+pwdInput.onkeyup = () => { 
+    console.log(pwdInput.value);  
+ if (pwdInput.value.length >= 8) {
+     console.log("length %o", length)
+     length.classList.remove('invalid')
+     length.classList.add('is-valid')
+ }
+}
