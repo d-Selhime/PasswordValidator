efb2f34470597000cd20b6109d3e54eab1b23e0c
daveSelhime <41130208+daveSelhime@users.noreply.github.com>
2019-12-27 16:54:00 -0700

    added svg sprites, clear function

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const port = 3000;
 
 app.use(express.static(__dirname + '/public'))
 
-app.post('/createPassword', function (req, res) {
-    res.send(console.log(`Password entered`));
-})
+// app.post('/createPassword', function (req, res) {
+//     res.send(console.log(`Password entered`));
+// })
 
 app.listen(port, () => console.log(`My app is alive on port ${port}!`));
 
--- public/JS/validate.js
+++ public/JS/validate.js
@@ -1,28 +1,70 @@
 let validationBox =  document.querySelector('.validation-box');
-console.log(validationBox)
+// console.log(validationBox)
+
 // get password input on focus
 let pwdInput = document.querySelector('#password');
-console.log(pwdInput)
+// console.log(pwdInput)
 let characters = document.querySelector('.character');
 let length = document.querySelector('.length');
 let number = document.querySelector('.number');
 
 
-// add display block to validation-box
+// initially hide validation box and show onfocus 
 pwdInput.onfocus = function() {
-    validationBox.style.display = "block"
+    validationBox.classList.remove('validation-box')
+    validationBox.classList.add('validate-box')
 };
-// add display none to validation box on blur
-pwdInput.onblur = function() {
- validationBox.style.display = "none"
-};
-
-// When user types into input
-pwdInput.onkeyup = () => { 
-    console.log(pwdInput.value);  
+// When user types into input check for a min of 8 characters
+const valid = pwdInput.onkeyup = () => { 
  if (pwdInput.value.length >= 8) {
-     console.log("length %o", length)
      length.classList.remove('invalid')
      length.classList.add('is-valid')
  }
+// check for at least 1 number
+let nums = /[0-9]/g;
+ if (pwdInput.value.match(nums)) {
+    number.classList.remove('invalid')
+    number.classList.add('is-valid')
 }
+//check for special character
+let specChar = /[!@#$%^&*(),.?":{}|<>]/g;
+ if (pwdInput.value.match(specChar)) {
+     characters.classList.remove('invalid')
+     characters.classList.add('is-valid')
+ }
+}
+
+//clear input on button click
+const clearInput = document.getElementById('reset').addEventListener('click', function () {
+    pwdInput.value = ""
+    
+})
+
+//show password on eye click
+// const eyeIcon = document.querySelector('.eyeIcons')
+const icons = document.querySelector('.icon')
+icons.addEventListener('click', function () {
+    const eyeOff = document.querySelector('.icon__eye-off')
+    const eyeOn = document.querySelector('.icon__eye-on')
+    
+  console.log('button clicked');
+  eyeOff.classList.remove('is-active')
+  eyeOff.classList.add('is-hidden')
+   if (eyeOff.classList.contains('is-hidden')) {
+       eyeOn.classList.add('is-active')
+       pwdInput.removeAttribute('type')
+   } else  {
+       //not working
+        eyeOn.classList.remove('is-active')
+        eyeOn.classList.add('is-hidden')
+        eyeOff.classList.add('is-active')
+        pwdInput.setAttribute('type', 'password')
+   }
+
+
+   
+
+// pwdInput.removeAttribute(type)
+
+
+})
